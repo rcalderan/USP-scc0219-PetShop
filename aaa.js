8830b0e04caf5f344faba2d47a6737106ce2c8fb
Richard Calderan <richardcck@usp.bRichard Calderan>
2019-10-29 17:33:22 -0300

    Primeiro commit

---
+++ aaa.js
@@ -0,0 +1,3 @@
+function alll(){
+    alert('aaaaalll');
+}
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <div id="header">
+        Header...
+        </div>
+        <div id="nav">
+            nav...
+        </div>
+        
+        <div id="content">
+          content...          
+        </div>
+
+        <div id="foote">
+        </div>
+    </div>
+</template>
+
+<script>
+    alert('aaaappp');
+    export default {
+    }
+</script>
+
+<style>
+
+</style>
