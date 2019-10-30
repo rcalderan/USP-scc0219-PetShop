746a027e384ef8dbfc732f6435c2f0e90ac87231
Richard <richardcck@hotmail.com>
2019-10-30 08:34:25 -0300

    @vue/cli instalado

--- aaa.js
+++
@@ -1,3 +0,0 @@
-function alll(){
-    alert('aaaaalll');
-}
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-    <div id="app">
-        <div id="header">
-        Header...
-        </div>
-        <div id="nav">
-            nav...
-        </div>
-        
-        <div id="content">
-          content...          
-        </div>
-
-        <div id="foote">
-        </div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    alert('aaaappp');
-    export default {
-    }
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
